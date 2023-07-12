f4c53ccd54a02002388e5ee0f0cbeb25def2344f
Nero <karta0706566@gmail.com>
2023-07-12 09:15:43 +0800

    feat: add express-handlebars

--- app.js
+++ app.js
@@ -8,12 +8,16 @@ const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const session = require('express-session')
 
+// routes
+const routes = require('./routes')
+
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.get('',(req,res)=>{
-  return res.send('Succeed in listening.')
-})
+app.engine('hbs',exphbs.engine({extname:'.hbs',defaultLayout:'main'}))
+app.set('view engine','hbs')
+
+app.use(routes)
 
 app.listen(PORT,()=>console.log(`App execute on port${PORT}.`))
 

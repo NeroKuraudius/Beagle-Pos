0a3f09ae05676e5170d099c59a94b10321e1ba9d
Nero <karta0706566@gmail.com>
2023-07-15 15:04:41 +0800

    feat: add drinks pagination

--- contorllers/drinks.js
+++ contorllers/drinks.js
@@ -4,23 +4,24 @@ const { getOffset, getPagination } = require('../helpers/pagination-helpers')
 const drinkController = {
   // 前台操作首頁
   getDrinks: async (req, res, next) => {
-    // const MAX_PAGE = 4
-    // const page = parseInt(req.query.page) || 1
-    // const limit = parseInt(req.query.limit) || MAX_PAGE
-    // const offset = getOffset(limit, page)
+    const limit = 5
+    const page = parseInt(req.query.page) || 1
+    const offset = getOffset(limit, page)
 
     const categoryId = parseInt(req.query.categoryId) || ''
-    try{
-    const categories = await Category.findAll({ raw: true, nest: true })
-    const drinks = await Drink.findAll({ 
-      raw: true, 
-      nest: true,
-      where:{...categoryId ? { categoryId } : {}}
-     })
+    try {
+      const categories = await Category.findAll({ raw: true, nest: true })
+      const drinks = await Drink.findAndCountAll({
+        raw: true,
+        nest: true,
+        where: { ...categoryId ? { categoryId } : {} },
+        limit,
+        offset
+      })
+      const pagination = getPagination(limit, page, drinks.count)
 
-    console.log(drinks)
-    return res.render('drinks', { categoryId,categories,drinks })
-    }catch(err){
+      return res.render('drinks', { categoryId, categories, drinks: drinks.rows, pagination })
+    } catch (err) {
       console.error(`Data search error: ${err}`)
     }
   }

4bc5cc8a0ad157c0a8184a20b1e0a4b5f4f3393a
Nero <karta0706566@gmail.com>
2023-07-16 10:41:24 +0800

    feat: add toppings model & seed

---
+++ models/topping.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Topping extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Topping.init({
+    name: DataTypes.STRING,
+    price: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Topping',
+    tableName: 'Toppings',
+    underscored: true,
+  });
+  return Topping;
+};
---
+++ seeders/20230716023642-create-toppings-seed.js
@@ -0,0 +1,60 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const toppings = [
+      {
+        name: '珍珠',
+        price: 10,
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        name: '寒天',
+        price: 10,
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        name: '蜂蜜棟',
+        price: 10,
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        name: '芋園',
+        price: 15,
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        name: '布丁',
+        price: 15,
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        name: '冰淇淋',
+        price: 15,
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+    ]
+    try {
+      await queryInterface.bulkInsert('Toppings', toppings, {})
+      console.log('Toppings seed created successfully!')
+    } catch (err) {
+      console.error(`Toppings seed creat error: ${err}`)
+    }
+  },
+
+  async down(queryInterface, Sequelize) {
+    try {
+      await queryInterface.bulkDelete('Toppings', {})
+      console.log('Toppings seed deleted successfully!')
+    } catch (err) {
+      console.error(`Toppings seed delete error: ${err}`)
+    }
+  }
+};

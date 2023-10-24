dd4c427603fe34bc453ea89b13c074cc823d1473
Nero <karta0706566@gmail.com>
2023-10-24 21:10:11 +0800

    feat: add jwt-passport

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,15 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
+const passportJWT = require('passport-jwt')
 const bcrypt = require('bcryptjs')
 const { User } = require('../models')
+
+const JWTStrategy = passportJWT.Strategy
+const ExtractJWT = passportJWT.ExtractJwt
+const jwtOptions = {
+  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.SECRET
+}
 
 // 前台登入
 passport.use('staffLogin', new LocalStrategy({
@@ -20,7 +28,8 @@ passport.use('staffLogin', new LocalStrategy({
 
     return cb(null, user)
   } catch (err) {
-    cb(`Passport authenticated error: ${err}`)
+    console.error(`Passport staffLogin authenticated error: ${err}`)
+    return cb(err)
   }
 }
 )
@@ -48,6 +57,16 @@ passport.use('ownerLogin', new LocalStrategy({
   }
 }
 ))
+
+// JWT
+passport.use(new JWTStrategy(jwtOptions, async (payload, cb) => {
+  try {
+    const user = await User.findByPk(payload.id)
+    return cb(null, user)
+  } catch (err) {
+    return cb(err)
+  }
+}))
 
 passport.serializeUser((user, cb) => {
   cb(null, user.id)

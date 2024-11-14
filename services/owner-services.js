aed9d10bb9f3f018b8b7c84188b0b04e5f9e87ac
giovanni.huang <giovanni.huang@gmail.com>
2024-11-14 12:46:03 +0800

    modify: change username into db_username in config & .env

--- services/owner-services.js
+++ services/owner-services.js
@@ -2,7 +2,7 @@ const { User, Shift, Income, Order, Consume,
   Drink, Ice, Sugar, Topping, Category } = require('../models')
 const bcrypt = require('bcryptjs')
 const { Sequelize } = require('sequelize')
-const sequelize = new Sequelize(process.env.DATABASE, process.env.USERNAME, process.env.PASSWORD, { host: process.env.HOST, dialect: 'mysql' })
+const sequelize = new Sequelize(process.env.DATABASE, process.env.DB_USERNAME, process.env.PASSWORD, { host: process.env.HOST, dialect: 'mysql' })
 
 const ownerServices = {
   getIncomes: async (req, cb) => {

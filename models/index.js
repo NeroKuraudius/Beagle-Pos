9fcf4b54a46ba501d7f589878544cce8b58eef3c
Nero <karta0706566@gmail.com>
2025-01-05 22:06:50 +0800

    modify: adjust the db connection in models/index.js

--- models/index.js
+++ models/index.js
@@ -5,15 +5,14 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = { host: process.env.HOST, dialect: 'mysql' }
 const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.db_username, config.password, config);
+  sequelize = new Sequelize(process.env.DATABASE, process.env.DB_USERNAME, process.env.PASSWORD, config);
 }
 
 fs

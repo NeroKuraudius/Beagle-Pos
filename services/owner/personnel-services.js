87a830b4582c28e5bc9267dea6f346d036c5a4aa
Nero Hsu <nero.hsu@inspiredgt.local>
2025-09-09 15:36:50 +0800

    refactor: adjsut transaction flow in owner/personnel-service

--- services/owner/personnel-services.js
+++ services/owner/personnel-services.js
@@ -6,13 +6,16 @@ const sequelize = new Sequelize(process.env.DATABASE, process.env.DB_USERNAME, p
 const personnelServices = {
   getStaffs: async (req, cb) => {
     try {
-      const admin = await User.findOne({ where: { id: req.user.id }, attributes: ['name'], raw: true })
-      const users = await User.findAll({
-        where: { role: 'staff' },
-        attributes: ['id', 'name'],
-        include: [{ model: Shift, attributes: ['name'] }],
-        raw: true, nest: true
-      })
+      const [ admin, users ] = await Promise.all([
+        await User.findOne({ where: { id: req.user.id }, attributes: ['name'], raw: true }),
+        await User.findAll({
+          where: { role: 'staff' },
+          attributes: ['id', 'name'],
+          include: [{ model: Shift, attributes: ['name'] }],
+          raw: true, nest: true
+        })
+      ])
+
       return cb(null, { users, admin })
     } catch (err) {
       return cb(err)
@@ -22,20 +25,24 @@ const personnelServices = {
   putStaff: async (req, cb) => {
     const id = parseInt(req.params.Uid)
     try {
-      const user = await User.findByPk(id)
-      if (!user) {
-        const err = new Error('查無該員工資料')
-        err.status = 404
-        throw err
-      }
+      const result = await sequelize.transaction(async(t)=>{
+        const user = await User.findByPk(id, { transaction: t })
+        if (!user) {
+          const err = new Error('查無該員工資料')
+          err.status = 404
+          throw err
+        }
 
-      if (user.toJSON().shiftId === 1) {
-        await user.update({ shiftId: 2 })
-      } else {
-        await user.update({ shiftId: 1 })
-      }
+        if (user.toJSON().shiftId === 1) {
+          await user.update({ shiftId: 2 }, { transaction: t })
+        } else {
+          await user.update({ shiftId: 1 }, { transaction: t })
+        }
+      })
+      
       return cb(null)
     } catch (err) {
+      if (!err.status) err.status = 500
       return cb(err)
     }
   },
@@ -50,13 +57,16 @@ const personnelServices = {
         throw err
       }
 
-      const admin = await User.findOne({ where: { id: req.user.id }, attributes: ['name'], raw: true })
-      const users = await User.findAll({
-        where: { role: 'staff' },
-        attributes: ['id', 'name'],
-        include: [{ model: Shift, attributes: ['name'] }],
-        raw: true, nest: true
-      })
+      const [ admin, users ] = await Promise.all([
+        await User.findOne({ where: { id: req.user.id }, attributes: ['name'], raw: true }),
+        await User.findAll({
+          where: { role: 'staff' },
+          attributes: ['id', 'name'],
+          include: [{ model: Shift, attributes: ['name'] }],
+          raw: true, nest: true
+        })
+      ])
+
       return cb(null, { admin, users, staff })
     } catch (err) {
       return cb(err)
@@ -65,69 +75,88 @@ const personnelServices = {
 
   patchStaffData: async (req, cb) => {
     const { name, phone, account, password, checkPassword } = req.body
+    const [ trimName, trimPhone, trimAccount, trimPwd, trimCheckPsw ] = [ name.trim(), phone.trim(), account.trim(), password.trim(), checkPassword.trim() ]
     const id = parseInt(req.params.Uid)
     try {
-      if (!name.trim() || !phone.trim() || !account.trim()) {
+      if (!trimName || !trimPhone || !trimAccount) {
         const err = new Error('缺少必填資料')
-        err.status = 404
+        err.status = 400
         throw err
       }
-      if (password !== checkPassword) {
+      if (trimPwd !== trimCheckPsw) {
         const err = new Error('密碼不一致')
-        err.status = 404
+        err.status = 400
         throw err
       }
 
-      const usedAccount = await User.findOne({ where: { account } }, { raw: true })
-      const editingUser = await User.findByPk(id, { raw: true })
-      if (usedAccount && (usedAccount.id !== editingUser.id)) {
-        const err = new Error('該帳號已被使用')
-        err.status = 404
-        throw err
-      }
-      const hash = await bcrypt.hash(password, 12)
-      await User.update({ name, phone, account, password: hash }, { where: { id } })
+      const result = await sequelize.transaction(async(t)=>{
+        const existingUserWithAccount = await User.findOne({
+            where: {
+                account: trimAccount,
+                id: { [Op.ne]: id } // Op.ne: Not equal (不等於)
+            },
+            transaction: t
+        })
+        if (existingUserWithAccount) {
+          const err = new Error('該帳號已被使用')
+          err.status = 400
+          throw err
+        }
+
+        const hash = await bcrypt.hash(password, 12)
+        await User.update({ name, phone, account, password: hash }, { where: { id }, transaction: t })
+      })
+      
       return cb(null)
     } catch (err) {
+      if (!err.status) err.status = 500
       return cb(err)
     }
   },
 
   createStaff: async (req, cb) => {
     const { name, phone, account, password, checkPassword, shiftId } = req.body
+    const [ trimName, trimPhone, trimAccount, trimPwd, trimCheckPsw ] = [ name.trim(), phone.trim(), account.trim(), password.trim(), checkPassword.trim() ]
+    
     try {
-      if (!name.trim() || !phone.trim() || !account.trim() || !shiftId || !password.trim() || !checkPassword.trim()) {
+      if (!trimName || !trimPhone || !trimAccount || !shiftId || !trimPwd || !trimCheckPsw) {
         const err = new Error('所有欄位皆為必填')
-        err.status = 404
+        err.status = 500
         throw err
       }
-      if (password !== checkPassword) {
+      if (trimPwd !== trimCheckPsw) {
         const err = new Error('兩次輸入的密碼不相符')
-        err.status = 404
+        err.status = 500
         throw err
       }
 
-      const userdAccount = await User.findOne({ where: { account } })
-      const errorMsg = []
-      if (userdAccount) {
-        const users = await User.findAll({
-          where: { role: 'staff' },
-          attributes: ['id', 'name'],
-          include: [{ model: Shift, attributes: ['name'] }],
-          raw: true, nest: true
-        })
-        errorMsg.push('該帳號已被使用')
-        const admin = await User.findOne({ where: { id: req.user.id }, attributes: ['name'], raw: true })
-        return cb(null, { errorMsg, admin, users, name, phone, account })
-      }
+      const result = await sequelize.transaction(async(t)=>{
+        const userdAccount = await User.findOne({ where: { account }, transaction: t })
 
-      const hash = await bcrypt.hash(password, 12)
-      let newUser = await User.create({ name, phone, account, shiftId, password: hash, role: 'staff' })
-      newUser = newUser.toJSON()
-      delete newUser.password
+        const errorMsg = []
+        if (userdAccount) {
+          const users = await User.findAll({
+            where: { role: 'staff' },
+            attributes: ['id', 'name'],
+            include: [{ model: Shift, attributes: ['name'] }],
+            raw: true, nest: true, transaction: t
+          })
+          errorMsg.push('該帳號已被使用')
+          const admin = await User.findOne({ where: { id: req.user.id }, attributes: ['name'], raw: true, transaction: t })
+          return { errorMsg, admin, users, name, phone, account }
+        }
 
-      return cb(null, { errorMsg, newUser })
+        const hash = await bcrypt.hash(password, 12)
+        let newUser = await User.create({ name, phone, account, shiftId, password: hash, role: 'staff' }, { transaction: t })
+        newUser = newUser.toJSON()
+        delete newUser.password
+
+        return { newUser }
+      })
+
+      return cb(null, result)
     } catch (err) {
+      if (!err.status) err.status = 500
       return cb(err)
     }
   },
@@ -135,25 +164,30 @@ const personnelServices = {
   deleteStaff: async (req, cb) => {
     const id = parseInt(req.params.Uid)
     try {
-      const user = await User.findByPk(id)
-      if (!user) {
-        const err = new Error('查無該員工資料')
-        err.status = 404
-        throw err
-      }
-      if (user.toJSON().name.includes('(已離職)')) {
-        const err = new Error('該員工已離職')
-        err.status = 404
-        throw err
-      }
+      const result = await sequelize.transaction(async(t)=>{
+        const user = await User.findByPk(id, { transaction: t })
+        if (!user) {
+          const err = new Error('查無該員工資料')
+          err.status = 404
+          throw err
+        }
+        if (user.toJSON().name.includes('(已離職)')) {
+          const err = new Error('該員工已離職')
+          err.status = 400
+          throw err
+        }
 
-      const name = user.toJSON().name + '(已離職)'
-      let deletedUser = await user.update({ role: 'quitted', name })
-      deletedUser = deletedUser.toJSON()
-      delete deletedUser.password
+        const quittedName = user.toJSON().name + '(已離職)'
+        let deletedUser = await user.update({ role: 'quitted', name: quittedName }, { transaction: t })
+        deletedUser = deletedUser.toJSON()
+        delete deletedUser.password
 
-      return cb(null, { deletedUser })
+        return { deletedUser }
+      })
+
+      return cb(null, result)
     } catch (err) {
+      if (!err.status) err.status = 500
       return cb(err)
     }
   }
